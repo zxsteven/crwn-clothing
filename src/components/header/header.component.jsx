4b9303737591dbcef9e58d15ce7b67891c9f0866
zxsteven <breathesteve@gmail.com>
2019-10-02 01:26:40 -0500

    added cartIcon and cartDropdown components to app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -19,16 +21,22 @@ const Header = ({ currentUser }) => (
       </Link>
       {
         currentUser ?
-        <div className='opition' onClick={() => auth.signOut()}>Sign Out</div>
+        (<div className='opition' onClick={() => auth.signOut()}>Sign Out</div>)
         :
-        <Link className='option' to='/signin'>Sign In</Link>
+        (<Link className='option' to='/signin'>Sign In</Link>)
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? null :
+      <CartDropdown />
+    }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header)
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,7 +32,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       })
     } catch (error) {
-      console.log('eror creating user', error.message)
+      console.log('error creating user', error.message)
     }
   }
   return userRef
